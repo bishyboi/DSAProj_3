7037e9e4496cac604cb667d769d65c736a6bc3c2
Bishoy P <bishoypramanik@gmail.com>
2023-08-03 22:56:34 -0400

    df update

--- python/dataframe.py
+++ python/dataframe.py
@@ -1,10 +1,13 @@
 import pandas as pd
-
+import numpy as np
 df = pd.read_csv('../output_extra_clean/output_extra_clean1950.csv')
 df.columns = ['explicit', 'language', 'year']
 
-languages = pd.unique(df['language'])
 
-with open("languages.txt", "w") as file:
-    for language in languages:
-        file.write(f"{language}\n")
+# print(np.sort(pd.unique(df['year'])))
+
+# languages = pd.unique(df['language'])
+
+# with open("languages.txt", "w") as file:
+#     for language in languages:
+#         file.write(f"{language}\n")
