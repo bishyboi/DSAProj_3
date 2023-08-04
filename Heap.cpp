34a3f805465bb49482fd6e77968c677fe0df4939
Jonathan Gaskin-Paulsen <122717840+Indigentharp291@users.noreply.github.com>
2023-08-03 22:38:37 -0400

    Add files via upload

---
+++ Heap.cpp
@@ -0,0 +1,54 @@
+#include "Heap.h"
+
+Heap::Heap(int capacity_) 
+{
+    capacity = capacity_;
+    list = new node[capacity];
+    size = 0;
+}
+
+void Heap::insert(song s){
+    bool emplaced = false; //Boolean to see if a song was emplaced in a list.
+    for (int i = 0; i < size; i++) {
+        if (s.year == list[i].year) { //if a given year exist, emplace the song data at the given node and stop the loop
+            list[i].songList.push_back(s);
+            emplaced = true;
+        }
+        if (emplaced)
+        {
+            break;
+        }
+    }
+    if (!emplaced) //if a song has not been emplaced, make a new node for that year
+    {
+        node tempNode;
+        tempNode.year = s.year;
+        tempNode.songList.push_back(s);
+        list[size] = tempNode;
+        size++;
+        cout << "emplaced " << list[size - 1].year << endl;
+    }
+    if (size > 1)
+    {
+        heapifyUp(size - 1);
+    }
+}
+
+void Heap::print()
+{
+    for (int i = 0; i < size; i++) {
+        cout << "Song data for: " << list[i].year << endl;
+        for (unsigned int j = 0; j < list[i].songList.size(); j++) {
+            cout << list[i].songList[j].language << " " << list[i].songList[j].ExpLyrics << endl;
+        }
+    }
+}
+
+void Heap::heapifyUp(int index)
+{
+    int parent = (index - 1) / 2;
+    if (list[index].year > list[parent].year) {
+        swap(list[index], list[parent]);
+        heapifyUp(parent);
+    }
+}
---
+++ Heap.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+#include "song.h"
+#include <chrono>
+#include <tuple>
+
+//Max Heap
+class Heap {
+    struct node
+    {
+        int year = 0;
+        vector<song> songList;
+    };
+
+    int capacity; //capacity of the heap
+    int size; //current size of the heap
+    node* list; //heap data structure
+public:
+    Heap(int capacity_);
+
+    void insert(song s);
+
+    void print(); //for debugging and testing
+
+
+    void heapifyUp(int index); //helper function to do the heap work;
+
+
+
+};
